d2ae7ff9ed7595ca19af8b89f694ddbf84aea1db
ShaunsTestAutomation <shaun.williams.testautomation@outlook.com>
2018-10-05 12:22:55 +0100

    Made myContentHandler generic to the calling classes. Added a new calling class called CallSax.class. Moved resources into a single common folder.

--- src/CallSax.java
+++ src/CallSax.java
@@ -1,16 +1,18 @@
-
 import javax.xml.parsers.*;
 import org.xml.sax.*;
 import java.io.*;
 
-public class CountSheep {
-    static public void main(String[] args) throws Exception {
+public class CallSax {
+    static public void main(String[] args) throws SAXException, ParserConfigurationException, IOException {
         SAXParserFactory factory = SAXParserFactory.newInstance();
         SAXParser saxParser = factory.newSAXParser();
         XMLReader xmlReader = saxParser.getXMLReader();
 
         xmlReader.setContentHandler(new myContentHandler());
 
+        xmlReader.parse(new File("..\\..\\..\\Resources\\Sins.xml").toString());
+        //Don't forget that now we have declared our parser (xmlReader) we could re-use it over and over again
+        //e.g.
         xmlReader.parse(new File("..\\..\\..\\Resources\\Sheep.xml").toString());
     }
 }
--- src/myContentHandler.java
+++ src/myContentHandler.java
@@ -13,23 +13,38 @@ import org.xml.sax.Attributes;
 public class myContentHandler extends DefaultHandler {
     private int count = 0;
 
+    public void startDocument(){
+        System.out.println("Starting the document");
+    }
     public void startElement(String uri,
                              String localName,
                              String qualNam,
                              Attributes attribs) {
-        if (qualNam.equals("Sheep") || qualNam.equals("sheep"))
-            System.out.println(++count + " qualNam:" + qualNam + " localName:" + localName + "Attribs:" + attribs.getLength());
-        //Checking to see whehter there are any attritute values and print them to stdout if there are any
-        if (attribs.getLength() > 0) {
+            System.out.print("Start tag: ");
+            System.out.println(qualNam);
             for (int i = 0; i < attribs.getLength(); i++) {
+                System.out.print("Attribute: ");
+                System.out.print(attribs.getQName(i));
+                System.out.print(" = ");
                 System.out.println(attribs.getValue(i));
             }
-        }
-        System.out.println();
     }
 
+    public void characters(char[] charArray, int start, int length){
+        String charString = new String(charArray, start, length);
+        charString = charString.replaceAll("\n", "[cr]");
+        charString = charString.replaceAll(" ", "[blank]");
+        System.out.println(length + "characters: ");
+        System.out.println(charString);
+    }
 
+    public void endElement(String Uri,
+                           String localName,
+                           String qualName){
+        System.out.print("End Tag: ");
+        System.out.println(qualName);
+    }
     public void endDocument(){
-        System.out.println("Sleep tight!");
+        System.out.println("Sleep tight!  but Notice that I'm case sensitive too");
     }
 }
